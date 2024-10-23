8d2a70ceeef6c7f47a2de8e0ac1997ac7795fa0c
zeff96 <zeffo1975@gmail.com>
2024-10-23 13:38:37 +0300

    Add fix for eslint errors

--- components/about.tsx
+++ components/about.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 
-import SectionHeading from './section-heading'
-import { motion } from 'framer-motion'
-import { useSectionInView } from '@/lib/hooks'
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function About() {
-  const { ref } = useSectionInView("About")
+  const { ref } = useSectionInView("About");
 
   return (
     <motion.section
@@ -18,16 +18,23 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        Having dedicated extensive time to immersive remote development, I've successfully crafted a diverse range of projects, spanning from impactful landing pages to intricate API integrations. My proficiency in JavaScript, Ruby, and Ruby on Rails is underscored by a track record of accomplishments. I've spent the last several months working remotely and asynchronously with people across three different time zones (UTC-6, UTC+1, UTC+5), which sets me apart.
+        Having dedicated extensive time to immersive remote development,
+        I&apos;ve successfully crafted a diverse range of projects, spanning
+        from impactful landing pages to intricate API integrations. My
+        proficiency in JavaScript, Ruby, and Ruby on Rails is underscored by a
+        track record of accomplishments. I&apos;ve spent the last several months
+        working remotely and asynchronously with people across three different
+        time zones (UTC-6, UTC+1, UTC+5), which sets me apart.
       </p>
 
       <p>
-        <span className="italic">When I&apos;m not coding</span>, I enjoy playing
-        video games, reading books, and conversing with my beloved ones. I also enjoy&nbsp;
+        <span className="italic">When I&apos;m not coding</span>, I enjoy
+        playing video games, reading books, and conversing with my beloved ones.
+        I also enjoy&nbsp;
         <span className="font-medium">learning new things</span>. I am currently
         learning about&nbsp;
         <span className="font-medium">learning how to play the guitar.</span>
       </p>
     </motion.section>
-  )
+  );
 }
